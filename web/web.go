3d5f4ccc272ffcc8d786678f3fc4d242ada8c18b
justyn <justyntemme@gmail.com>
2016-08-14 17:54:32 -0500

    finished web server

--- web/web.go
+++ web/web.go
@@ -1,9 +1,21 @@
-package main 
+package web
 
 import (
-	"fmt"
+    "fmt"
+    "net/http"
 )
 
-func main() {
-	fmt.Println("hello world")
+func Run() {
+    panic(http.ListenAndServe(":8080", &logServer{
+        hdl: http.FileServer(http.Dir("public/")),
+    }))
+}
+
+type logServer struct {
+    hdl http.Handler
+}
+
+func (l *logServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r.RemoteAddr, r.URL, r.URL.Path)
+    l.hdl.ServeHTTP(w, r)
 }
