451b5921a2ed8c4d1754474c38ffc7a87a01ce03
Justyn Temme <Justyntemme@gmail.com>
2017-09-28 18:30:51 -0500

    fix for new directory tree

--- web/web.go
+++ web/web.go
@@ -1,21 +1,22 @@
 package web
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func Run() {
-    panic(http.ListenAndServe(":8080", &logServer{
-        hdl: http.FileServer(http.Dir("/go/src/github.com/justyntemme/nextwave/public/")),
-    }))
+	panic(http.ListenAndServe(":8080", &logServer{
+		hdl: http.FileServer(http.Dir("/go/src/github.com/justyntemme/nextwave/nextwave/public/")),
+	}))
 }
 
 type logServer struct {
-    hdl http.Handler
+	hdl http.Handler
 }
 
 func (l *logServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r.RemoteAddr, r.URL, r.URL.Path)
-    l.hdl.ServeHTTP(w, r)
+	fmt.Println(r.RemoteAddr, r.URL, r.URL.Path)
+	l.hdl.ServeHTTP(w, r)
 }
+
